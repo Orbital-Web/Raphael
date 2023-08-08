51041bbdf2477fd57a4775d69802d909f681d6b5
Orbital-Web <36625832+Orbital-Web@users.noreply.github.com>
2023-08-08 20:39:14 +0900

    feat: added more description to game summary

--- src/Games/summarize.py
+++ src/Games/summarize.py
@@ -1,14 +1,21 @@
 import chess.pgn
 
-p1 = 0
-draw = 0
-p2 = 0
+# [p2, p1]
+results = {
+    "white wins": [0, 0],
+    "black wins": [0, 0],
+    "timeout wins": [0, 0],
+    "stalemate draws": 0,
+    "repetition draws": 0,
+    "other draws": 0
+}
 p1_is_white = False
 
 with open("../../logs/compare.pgn", "r") as pgn:
-    for i in range(400):
+    while True:
         p1_is_white = not p1_is_white
-        game = chess.pgn.read_game(pgn)
+        if ((game:=chess.pgn.read_game(pgn)) is None):
+            break
         moves = game.mainline_moves()
         board = game.board()
         
@@ -16,15 +23,27 @@ with open("../../logs/compare.pgn", "r") as pgn:
             board.push(move)
         
         outcome = board.outcome()
-        if outcome == None:
-            draw += 1
+        if outcome == None: # no outcome, must be timeout (player on turn loses)
+            results["timeout wins"][board.turn!=p1_is_white] += 1
             continue
         
-        outcome = outcome.winner
-        if outcome != None:
-            p1 += (outcome == p1_is_white)
-            p2 += (outcome == (not p1_is_white))
+        if outcome.winner != None:
+            if outcome.winner:  # white won, raise score of white player
+                results["white wins"][p1_is_white] += 1
+            else:               # black won, raise score of black player
+                results["black wins"][~p1_is_white] += 1
         else:
-            draw += 1
+            if outcome.termination == chess.Termination.FIVEFOLD_REPETITION:
+                results["repetition draws"] += 1
+            elif outcome.termination == chess.Termination.STALEMATE:
+                results["stalemate draws"] += 1
+            else:
+                results["other draws"] += 1
             
-print(f"{p1=}\n{draw=}\n{p2=}")
+p1_wins = results["white wins"][1] + results["black wins"][1] + results["timeout wins"][1]
+p2_wins = results["white wins"][0] + results["black wins"][0] + results["timeout wins"][0]
+draws = results["stalemate draws"] + results["repetition draws"] + results["other draws"]
+
+print(f"p1: {p1_wins} [white: {results['white wins'][1]}, black: {results['black wins'][1]}, timeout: {results['timeout wins'][1]}]")
+print(f"p2: {p2_wins} [white: {results['white wins'][0]}, black: {results['black wins'][0]}, timeout: {results['timeout wins'][0]}]")
+print(f"Dr: {draws} [rep: {results['repetition draws']}, stale: {results['stalemate draws']}, other: {results['other draws']}]")

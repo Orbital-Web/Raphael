ffe3472a2ed9c5c07de36b8b1594fd4e14906cc8
Rei Meguro <36625832+Orbital-Web@users.noreply.github.com>
2026-03-23 11:07:12 -0400

    feat: uci wait and corrected static eval
    bench: 9164365

--- src/Raphael/Raphael.cpp
+++ src/Raphael/Raphael.cpp
@@ -200,7 +200,10 @@ void Raphael::ponder(atomic<bool>& halt) {
 }
 
 
-i32 Raphael::static_eval() { return position_.evaluate(); }
+i32 Raphael::static_eval(bool corrected) {
+    const auto raw_score = position_.evaluate();
+    return (corrected) ? history_.correct(position_.board(), raw_score) : raw_score;
+}
 
 
 void Raphael::reset() {

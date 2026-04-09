bdfa54a44123422cad6f198a2cab9e75ca3746f1
Rei Meguro <36625832+Orbital-Web@users.noreply.github.com>
2026-04-08 20:28:03 -0400

    docs: update

--- src/Raphael/Raphael.cpp
+++ src/Raphael/Raphael.cpp
@@ -27,7 +27,7 @@ using std::swap;
 
 
 
-const string Raphael::version = "3.3.0";
+const string Raphael::version = "4.0.0-dev";
 
 const Raphael::EngineOptions& Raphael::default_params() {
     static EngineOptions opts{

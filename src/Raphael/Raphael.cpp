1548e6cd7e7a8a1222958e6206b5b6c4990e2df5
Rei Meguro <36625832+Orbital-Web@users.noreply.github.com>
2026-05-15 14:45:38 -0400

    take 3
    bench: 8811102

--- src/Raphael/Raphael.cpp
+++ src/Raphael/Raphael.cpp
@@ -497,7 +497,7 @@ i32 Raphael::negamax(
         // reverse futility pruning
         const i32 rfp_margin = RFP_MARGIN_DEPTH_MUL * fdepth / DEPTH_SCALE
                                - improving * RFP_MARGIN_IMPROVING
-                               + corrplexity * RFP_MARGIN_CORRPLEXITY / 128;
+                               + corrplexity * RFP_MARGIN_CORRPLEXITY / 1024;
         if (fdepth <= RFP_MAX_DEPTH && score_estimate - rfp_margin >= beta) return score_estimate;
 
         // razoring

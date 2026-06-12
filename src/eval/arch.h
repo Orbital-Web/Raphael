bef753964453a94b7c7c4462be8040f183604157
Rei Meguro <36625832+Orbital-Web@users.noreply.github.com>
2026-06-11 20:59:21 -0700

    feat: higher wdl finetuning
    bench: 10998114

--- src/eval/arch.h
+++ src/eval/arch.h
@@ -5,7 +5,7 @@
 
 
 namespace raphael::nnue {
-static constexpr i32 OUTPUT_SCALE = 276;
+static constexpr i32 OUTPUT_SCALE = 253;
 static constexpr i32 N_PSQ = 11 * 64;
 static constexpr i32 N_THREATS = 60144;
 static constexpr i32 L1_SIZE = 768;
